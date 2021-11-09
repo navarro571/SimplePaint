4bef4f72c5f8a1b574220d0334b99f10c0654e8d
navarro571 <navarro571g@gmail.com>
2021-11-09 01:51:11 -0300

    New brush, RectBrush

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,23 @@ import CanvasMouseEvent from "./canvas/CanvasMouseEvent";
 import DefaultBrush from "./brushes/DefaultBrush";
 import GreenBrush from "./brushes/GreenBrush";
 import ButtonHandler from './butons/ButtonHandler';
+import './style.css';
+import RectBrush from "./brushes/RectBrush";
 
 const htmlCanvas: HTMLCanvasElement = document.querySelector("canvas");
 
 const canvas = new Canvas(htmlCanvas, { height: 800, width: 600 });
 const brushHandler = BrushHandler.getInstance()
-let pincel1 = new DefaultBrush()
-let pincel2 = new GreenBrush()
+let pincel1 = new DefaultBrush();
+let pincel2 = new GreenBrush();
+let pincel3 = new RectBrush();
 
 brushHandler.setBrush(pincel2)//temporalmente
 
 let buttonHandler = new ButtonHandler()
 buttonHandler.registerButton(document.querySelector('#red'), pincel1)
 buttonHandler.registerButton(document.querySelector('#green'), pincel2)
+buttonHandler.registerButton(document.querySelector('#rect-brush'), pincel3)
 
 
 const canvasMouseEvent = new CanvasMouseEvent(canvas.canvas, canvas.context)
